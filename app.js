8dab8c621130a056054d19fb22aa2100d0f8cae5
Sakshij2810 <sakshi2020jadhav@gmail.com>
2024-12-28 19:01:08 +0530

    ndnmkdw

--- app.js
+++ app.js
@@ -6,23 +6,26 @@ import cors from "cors";
 config({ path: "./config/config.env" });
 export const app = express();
 
-// Add this configuration to allow specific origins
 const corsOptions = {
   origin: [
-    "http://localhost:3000",
-    "https://razorpay-client-hazel.vercel.app/",
-  ], // Add your frontend's URL
-  methods: ["GET", "POST"], // Allow specific methods
-  allowedHeaders: ["Content-Type"], // Allow specific headers
+    "https://razorpay-client-hazel.vercel.app", // Allow Vercel frontend
+    "http://localhost:3000", // Allow local development
+  ],
+  methods: ["GET", "POST"], // Allow specific HTTP methods
+  credentials: true, // Allow credentials if needed
 };
 
 app.use(cors(corsOptions)); // Use the CORS middleware
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", paymentRoute);
+
+app.get("/", () => {
+  console.log("server is running...");
+});
 
 app.get("/api/getkey", (req, res) =>
   res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
